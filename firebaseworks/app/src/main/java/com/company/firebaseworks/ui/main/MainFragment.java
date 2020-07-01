f76c43b205a17582eb9bf9c7cb31d49af34a958e
qqwwsyz <jasonchezheming@gmail.com>
2020-07-01 03:22:39 -0400

    firebase

---
+++ firebaseworks/app/src/main/java/com/company/firebaseworks/ui/main/MainFragment.java
@@ -0,0 +1,66 @@
+package com.company.firebaseworks.ui.main;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.company.firebaseworks.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainFragment extends Fragment {
+
+    private static final String TAG = "MainActivity";
+    private MainViewModel mViewModel;
+    TextView firebasetv;
+
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.main_fragment, container, false);
+        firebasetv = (TextView) fragmentView.findViewById(R.id.firebaseText);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                firebasetv.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        return fragmentView;
+    }
+
+}
