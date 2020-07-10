e4e5fdb7e1a5ccac274dc650f88bccb9aa392b99
Guo Tang <guotang@GuodeMacBook-Air.local>
2020-07-10 17:55:01 +0800

    updated

--- GOGOFITNESS/app/src/main/java/com/example/gogofitness/MainActivity.java
+++ GOGOFITNESS/app/src/main/java/com/example/gogofitness/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +34,44 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Chat.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chat_item:
+                Intent intent = new Intent(MainActivity.this,Chat.class);
+                startActivity(intent);
+                break;
+            case R.id.help_item:
+                Toast.makeText(this, "email: happy@gogofitness.com", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.map_item:
+                Uri uri = Uri.parse("geo:38.899533, -77.036476");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+                break;
+            case R.id.share_item:
+                Intent intent5 = new Intent(Intent.ACTION_SEND);
+                intent5.setType("text/plain");
+                intent5.putExtra(Intent.EXTRA_TEXT, "Share the love");
+                startActivity(Intent.createChooser(intent5,"Share the love"));
+                startActivity(intent5);
+                break;
+            case R.id.about_us:
+                Toast.makeText(this, "Guo Tang and Zheming Chen", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+        }
+        return true;
     }
 
 
